647760f291d65c6acd1a44b0d4bdf0768e293892
alpomishkom <sunnatbey1@gmail.com>
2023-10-28 16:43:53 +0500

    Lisvie qilidi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:home_world_lar/home_task_minu/home_task.dart';
+import 'package:home_world_lar/home_task_minu/task_1.dart';
 import 'package:home_world_lar/splash_page/home_splash.dart';
 
 void main() {
@@ -16,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  SplashPage(),
+      home: SplashPage(),
     );
   }
 }
